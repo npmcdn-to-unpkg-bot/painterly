dbe5f35d3a24b4b5405126ec15ad59432281f479
Kathy Starr <KStarrMitche@student.bridgew.edu>
2016-04-29 10:29:44 -0400

    first commit

---
+++ public/js/landing.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  <aside class="col md 3" style="background:white" onmouseover="this.style.background='gray';" onmouseout="this.style.background='white';">
+   Jack and Jill went up the hill
+   To fetch a pail of water.
+   Jack fell down and broke his crown,
+   And Jill came tumbling after.
+   </aside>
+});
