752fa98dd3af22253d6abbae4cdf8042f3b109be
Kathy Starr <KStarrMitche@student.bridgew.edu>
2016-04-29 12:51:52 -0400

    test commit

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,8 +1,3 @@
 $( document ).ready(function() {
-  <aside class="col md 3" style="background:white" onmouseover="this.style.background='gray';" onmouseout="this.style.background='white';">
-   Jack and Jill went up the hill
-   To fetch a pail of water.
-   Jack fell down and broke his crown,
-   And Jill came tumbling after.
-   </aside>
+  $( "input[type='radio']" ).prop( "checked", true ).checkboxradio( "refresh" );
 });
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Painterly | a whole new art critique website' });
+
 });
 
 module.exports = router;
