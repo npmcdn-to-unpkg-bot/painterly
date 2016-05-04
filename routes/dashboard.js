4f406e89c7d3f7f5e5bf7cd697a9dfef8a27c709
Kathy Starr <KStarrMitche@student.bridgew.edu>
2016-05-04 09:32:05 -0400

    at this point I'm not even sure what this commit is for

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,6 +6,8 @@ var bcrypt = require('bcryptjs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // connect DB and read table assignments
+  pg.connect(process.env.DATABASE_URL + "?ssl=true", connectDB_showPosts(req,res,next));
   res.render('dashboard', { title: 'Painterly | a whole new art critique website', user: req.user});
 });
 
@@ -13,5 +15,30 @@ router.get('/logout', function(req, res){
     req.logout();
     res.redirect('/'); // Successful. redirect to localhost:3000/exam
 });
+
+
+function runQuery_showPosts(req, res, client, done, next) {
+  return function(err, result){
+    if (err) {
+      console.log("unable to query SELECT ");
+      next(err); // throw error to error.hbs. only for test purpose
+    }
+    else {
+      console.log(result);
+      res.render('notAdmin', {usernamep: result.rows.username, post: result.rows.description} );
+    }
+  };
+} // client.query
+
+function connectDB_showPosts(req, res, next) {
+  return function(err, client, done) {
+    if (err){ // connection failed
+      console.log("Unable to connect to database");
+      return next(err);
+    }
+    client.query('SELECT * FROM posts', runQuery_notAdmin(req, res, client, done, next));
+  };
+}
+
 
 module.exports = router;
